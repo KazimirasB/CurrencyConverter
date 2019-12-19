cb7728464b6facb8f8e8de0b6d9a6a28da99ac0e
kazimiras <kazimiras.bangaitis@bs2.lt>
2019-12-19 08:51:45 +0200

    Test

--- app/src/main/java/lt/akb/currency/converter/RateHolderEdit.kt
+++ app/src/main/java/lt/akb/currency/converter/RateHolderEdit.kt
@@ -5,14 +5,14 @@ import lt.akb.currency.database.Rate
 import lt.akb.currency.databinding.RateEditItemBinding
 
 class RateHolderEdit(private val binding: RateEditItemBinding, adapter: RatesAdapter) :
-        RecyclerView.ViewHolder(binding.root), IRatesBind {
-        init {
-            binding.adapter = adapter
-        }
+    RecyclerView.ViewHolder(binding.root), IRatesBind {
+    init {
+        binding.adapter = adapter
+    }
 
-        override fun bind(item: Rate) {
-            binding.item = item
-         }
+    override fun bind(item: Rate) {
+        binding.item = item
+    }
 
     override fun bind(rate: Rate, payloads: MutableList<Any>) {
         bind(rate)
